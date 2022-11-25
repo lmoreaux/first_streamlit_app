fdf04bde615d50ac1fef674884d03ff6825f5c1c
lmoreaux <49000423+lmoreaux@users.noreply.github.com>
2022-11-25 16:39:19 +0100

    Update streamlit_app.py
    adding snowflake connection test

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,9 @@ streamlit.dataframe(fruityvice_normalized)
 
 #The line shown below will tell your py file to use the library you added to the project
 import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
